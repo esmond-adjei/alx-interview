7794465c50d1978f14c22b5bbf50851d658d2e7c
esmond-adjei <xmongram@gmail.com>
2023-12-04 20:20:15 +0000

    modified some files

--- 0-lockboxes.py
+++ 0-lockboxes.py
@@ -1,5 +1,22 @@
+from collections import deque
+
 def canUnlockAll(boxes):
-    solvable = True
+    """
+    canUnlockAll: determines whether a list of boxes with each containing
+    keys can be opened
+    """
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
 
+    queue = deque([0])
 
-    return solvable
+    while queue:
+        current_box = queue.popleft()
+
+        for key in boxes[current_box]:
+            if not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
