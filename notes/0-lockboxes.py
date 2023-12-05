4e0f396f52fa07461560e7ef598ed96cd079b0b4
esmond-adjei <xmongram@gmail.com>
2023-12-05 12:35:58 +0000

    modified some files

--- notes/0-lockboxes.py
+++ notes/0-lockboxes.py
@@ -1,23 +1,31 @@
 from collections import deque
 
-
 def canUnlockAll(boxes):
     """
     canUnlockAll: determines whether a list of boxes with each containing
     keys can be opened
     """
+    print()
+    print(boxes)
+
     n = len(boxes)
     visited = [False] * n
     visited[0] = True
 
+    print("Initilized visited list: ", visited)
     queue = deque([0])
 
+    print("Initilized queue: ", queue)
     while queue:
         current_box = queue.popleft()
 
+        print("current box", current_box)
         for key in boxes[current_box]:
             if not visited[key]:
                 visited[key] = True
                 queue.append(key)
+                print("visited list: ", visited)
+                print("queue: ", queue)
 
+    print("Final visited: ", visited)
     return all(visited)
