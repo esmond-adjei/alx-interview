50fb1e76f19d53227be6c8b124c230ae6f5775ea
esmond-adjei <xmongram@gmail.com>
2023-12-22 03:50:21 +0000

    modified some files

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -43,5 +43,5 @@ if __name__ == '__main__':
 
         print_stats(stats, filesize)
     except KeyboardInterrupt:
-        print_stats(stats, filesize)
+        #print_stats(stats, filesize)
         raise
