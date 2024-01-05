abc34d4d48d8a81ff5a879f5457a647f8047237e
esmond-adjei <xmongram@gmail.com>
2024-01-05 00:42:42 +0000

    modified some files

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -18,13 +18,13 @@ def validUTF8(data):
                 return False
         else:
             if byte >> 7 == 0:
-                remaining_bytes = 0  # Single-byte character
+                remaining_bytes = 0
             elif byte >> 5 == 0b110:
-                remaining_bytes = 1  # Two-byte character
+                remaining_bytes = 1
             elif byte >> 4 == 0b1110:
-                remaining_bytes = 2  # Three-byte character
+                remaining_bytes = 2
             elif byte >> 3 == 0b11110:
-                remaining_bytes = 3  # Four-byte character
+                remaining_bytes = 3
             else:
                 return False
 
