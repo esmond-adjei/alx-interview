aa7ab0461fd3feb09fd607d186705272e0dd195a
esmond-adjei <xmongram@gmail.com>
2024-02-07 00:00:13 +0000

    modified some files - 2024-02-07 00:00:13

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,10 +9,10 @@ def island_perimeter(grid):
     if not grid:
         return 0
 
-    cols, rows = len(grid), len(grid[0])
+    rows, cols = len(grid), len(grid[0])
     perimeter = 0
-    for i in range(1, cols - 1):
-        for j in range(1, rows - 1):
+    for i in range(1, rows - 1):
+        for j in range(1, cols - 1):
             if grid[i][j] == 1:
                 perimeter += 4
                 if i > 0 and grid[i + 1][j] == 1:
