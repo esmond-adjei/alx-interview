67d0398cda6831db324d3ac37a762e0334e72064
esmond-adjei <xmongram@gmail.com>
2024-02-15 02:43:19 +0000

    modified some files - 2024-02-15 02:43:18

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python3
-"""
-prime game
-"""
+"""prime game"""
 
 
-def isPrime(n):
-    if n <= 1:
+def is_prime(num):
+    """Check if a number is prime."""
+    if num <= 1:
         return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
             return False
-        i += 6
     return True
 
 
 def isWinner(x, nums):
+    """Determine the winner of each game."""
     maria_wins = 0
     ben_wins = 0
+
     for n in nums:
-        prime_count = sum(1 for i in range(2, n + 1) if isPrime(i))
-        if prime_count % 2 == 0:
+        primes_count = sum(1 for num in range(2, n + 1) if is_prime(num))
+        if primes_count % 2 == 0:
             ben_wins += 1
         else:
             maria_wins += 1
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
+
+
+if __name__ == "__main__":
+    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
