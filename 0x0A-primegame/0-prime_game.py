7e89092ddb9ad462c328523ee901c941e5369088
esmond-adjei <xmongram@gmail.com>
2024-02-16 03:04:40 +0000

    Refactor prime game logic

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,36 +1,22 @@
-#!/usr/bin/python3
-"""prime game"""
-
-
-def is_prime(num):
-    """Check if a number is prime."""
-    if num <= 1:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-
 def isWinner(x, nums):
-    """Determine the winner of each game."""
-    maria_wins = 0
-    ben_wins = 0
+    """
+    Evaluates the winner of a prime game session within x rounds of play
+    """
+    if x < 1 or not nums:
+        return None
+    
+    def is_prime(num):
+        """Check if a number is prime."""
+        if num < 2:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
 
-    for n in nums:
-        primes_count = sum(1 for num in range(1, n + 1) if is_prime(num))
-        if primes_count % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
+    marias_wins = sum(1 for n in nums if is_prime(n))
 
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    if marias_wins == x // 2:
         return None
 
-
-if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(5, [7, 2, 1, 4, 3])))
+    return 'Maria' if marias_wins > x // 2 else 'Ben'
